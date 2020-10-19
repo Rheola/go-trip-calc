990347aa2430cf3d4b86c242599ad6a9170de814
Svetlana Okuneva <rheola@yandex.ru>
2020-10-19 15:13:59 +0300

    refactor response

--- app/restapi/response.go
+++ app/restapi/response.go
@@ -1,6 +1,31 @@
 package restapi
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type APIResponse struct {
 	Code    int32  `json:"code"`
 	Message string `json:"message"`
+}
+
+func ResponseBadRequest(w http.ResponseWriter, err error) {
+
+	resp := APIResponse{
+		Code:    http.StatusBadRequest,
+		Message: err.Error(),
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode(resp)
+}
+
+func ResponseInternalError(w http.ResponseWriter, err error) {
+
+	resp := APIResponse{
+		Code:    http.StatusInternalServerError,
+		Message: err.Error(),
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode(resp)
 }
