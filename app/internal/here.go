083a149e2c0cf4425f2d09049bf4495884157497
Svetlana Okuneva <rheola@yandex.ru>
2020-10-23 08:58:40 +0300

    here

--- app/internal/here.go
+++ app/internal/here.go
@@ -2,23 +2,17 @@ package internal
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/rheola/go-trip-calc/app/models"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 )
-
-type travelSummary struct {
-	duration int
-	length   int
-}
 
 type TravelSummary struct {
 	BaseDuration int
-	Duration     int
-	Length       int
+	Duration     uint
+	Length       uint
 }
 type section struct {
 	TravelSummary TravelSummary `json:"travelSummary"`
@@ -30,12 +24,11 @@ type answer struct {
 	Routes []route `json:"routes"`
 }
 
-func CalcRoute(params models.RouteParams) {
+func CalcRoute(params models.RouteParams) (*TravelSummary, error) {
 
 	req, err := http.NewRequest("GET", "https://router.hereapi.com/v8/routes", nil)
 	if err != nil {
-		log.Print(err)
-		os.Exit(1)
+		return nil, err
 	}
 
 	q := req.URL.Query()
@@ -46,39 +39,26 @@ func CalcRoute(params models.RouteParams) {
 	q.Add("apikey", "WOp3bI0eN2_gEG1ob-orRSViXwd-53mYAa_Vn8dyuMM")
 	req.URL.RawQuery = q.Encode()
 
-	fmt.Println(req.URL.String())
-
-	// https://router.hereapi.com/v8/routes?
-	//transportMode=car&
-	//return=travelSummary&
-	//apikey=WOp3bI0eN2_gEG1ob-orRSViXwd-53mYAa_Vn8dyuMM https://router.hereapi.com/v8/routes?transportMode=car&origin=52.5308,13.3847&destination=52.5323,13.3789&return=polyline,turnbyturnactions&apikey=WOp3bI0eN2_gEG1ob-orRSViXwd-53mYAa_Vn8dyuMM
-
 	resp, err := http.Get(req.URL.String())
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 	jsonBlob, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 
 	var answer answer
 	err = json.Unmarshal(jsonBlob, &answer)
 	if err != nil {
-		fmt.Println("error:", err)
-		return
+		return nil, err
 	}
-	fmt.Println(answer.Routes)
 	for _, route := range answer.Routes {
-		fmt.Println(route.Sections)
 		for _, section := range route.Sections {
-			fmt.Println(section.TravelSummary.Duration)
-			fmt.Println(section.TravelSummary.BaseDuration)
-			fmt.Println(section.TravelSummary.Length)
+			return &section.TravelSummary, nil
 		}
 	}
+	return nil, errors.New("TravelSummary not found")
 }
