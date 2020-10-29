29053501b6f824ad911b6c4b4cebb308de88c4df
Svetlana Okuneva <rheola@yandex.ru>
2020-10-29 09:40:15 +0300

    use config

--- app/internal/here.go
+++ app/internal/here.go
@@ -4,10 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/rheola/go-trip-calc/app/config"
 	"github.com/rheola/go-trip-calc/app/models"
 	"io/ioutil"
 	"net/http"
 )
+
+const API_URL string = "https://router.hereapi.com/v8/routes"
 
 type TravelSummary struct {
 	BaseDuration int
@@ -25,8 +28,8 @@ type answer struct {
 }
 
 func CalcRoute(params models.RouteParams) (*TravelSummary, error) {
-
-	req, err := http.NewRequest("GET", "https://router.hereapi.com/v8/routes", nil)
+	conf := config.New()
+	req, err := http.NewRequest("GET", API_URL, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -36,7 +39,7 @@ func CalcRoute(params models.RouteParams) (*TravelSummary, error) {
 	q.Add("origin", fmt.Sprintf("%.6f,%.6f", params.From.Lat, params.From.Lon))
 	q.Add("destination", fmt.Sprintf("%.6f,%.6f", params.To.Lat, params.To.Lon))
 	q.Add("return", "travelSummary")
-	q.Add("apikey", "WOp3bI0eN2_gEG1ob-orRSViXwd-53mYAa_Vn8dyuMM")
+	q.Add("apikey", conf.ApiKey)
 	req.URL.RawQuery = q.Encode()
 
 	resp, err := http.Get(req.URL.String())
